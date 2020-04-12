f065b9d95ef4c3a4febf8cc5ea3f04ffdc61f181
SasquatchSPE <tomekmakow@gmail.com>
2020-04-11 21:10:34 -0400

    eggs!

--- MPR/App_Start/BundleConfig.cs
+++ MPR/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace MPR
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/egg").Include(
+                "~/Content/egg.css"));
         }
     }
 }
---
+++ MPR/Controllers/EggController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MPR.Controllers
+{
+    public class EggController : Controller
+    {
+        // GET: Egg
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
