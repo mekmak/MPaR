9a10a7ca98cb99b71292d1e791fbab9ddf5f0dea
Tomek Makowiecki <tomekmakow@gmail.com>
2021-06-05 19:12:39 -0400

    Highlight meat sports winners; enable c# 7 features

--- MPR/Models/EspnGame.cs
+++ MPR/Models/EspnGame.cs
@@ -11,14 +11,21 @@ namespace MPR.Models
         public string AwayTeam { get; set; }
 
         public string HomeTeamScore { get; set; }
+        private int _homeTeamScore => int.TryParse(HomeTeamScore, out var score) ? score : 0;
         public string AwayTeamScore { get; set; }
+        private int _awayTeamScore => int.TryParse(AwayTeamScore, out var score) ? score : 0;
 
         public bool NotifyAway { get; set; }
+        public bool AwayTeamWon => IsOver && _awayTeamScore > _homeTeamScore;
         public bool NotifyHome { get; set; }
+        public bool HomeTeamWon => IsOver && _homeTeamScore > _awayTeamScore;
 
         public string Score { get; set; }
 
         public string Time { get; set; }
         public string TimeLink { get; set; }
+
+        public bool IsOver => Time == "Final";
+
     }
 }
