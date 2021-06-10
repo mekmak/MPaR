febea76c707979878fb76a8dddf06dd5c222e892
Tomek Makowiecki <tomekmakow@gmail.com>
2021-06-09 22:21:32 -0400

    Enabled swipes on mobile

--- MPR/App_Start/BundleConfig.cs
+++ MPR/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace MPR
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/hammerjs").Include(
+                      "~/Scripts/hammer.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootswatch/cyborg/bootstrap.css",
                       "~/Content/site.css"));
