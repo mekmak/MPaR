8171a64ddcde0d5c3ab23ad1f9252cd9be88c3a9
Tomasz Makowiecki <1435173+mekmak@users.noreply.github.com>
2024-11-29 22:33:42 -0500

    Added REAL F1 standings (#18)

--- MPR/Models/F1Standings.cs
+++ MPR/Models/F1Standings.cs
@@ -5,7 +5,9 @@ namespace MPR.Models
     public class F1Standings
     {
         public List<F1Team> TeamStandings {get;set;}
+        public List<RealF1Team> RealTeamStandings {get;set;}
         public List<F1Driver> DriverStandings {get;set;}
+        public List<RealF1Driver> RealDriverStandings {get;set;}
     }
 
     public class F1Team
@@ -15,11 +17,35 @@ namespace MPR.Models
         public int Points { get; set; }
     }
 
+    public class RealF1Team
+    {
+        public string Name { get; set; }
+        public int Position {get;set;}
+        public int FakePosition { get; set; }
+        public int PositionsMoved { get; set; }
+        public double Points { get; set; }
+        public double PointsDiff { get; set; }
+        public int FakePoints { get; set; }
+    }
+
     public class F1Driver
     {
         public string Name { get; set; }
         public int Position { get; set; }
         public int Points { get; set; }
+        public string Nationality { get; set; }
+        public string Car { get; set; }
+    }
+
+    public class RealF1Driver
+    {
+        public string Name { get; set; }
+        public int Position { get; set; }
+        public int FakePosition { get; set; }
+        public int PositionsMoved { get; set; }
+        public double Points { get; set; }
+        public int FakePoints { get; set; }
+        public double PointsDiff { get; set; }
         public string Nationality { get; set; }
         public string Car { get; set; }
     }
